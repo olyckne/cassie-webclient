a85d09a69c730c485aee4ec7f70cdf888e608ebd
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-01-16 20:10:58 +0100

    Login view handles status error

--- application/views/login.blade.php
+++ application/views/login.blade.php
@@ -7,6 +7,11 @@
 
 @section("content")
 <div class="login hero-unit">
+	@if ( Session::has("status") )
+		<div class="alert alert-error">
+			{{ Session::get("status") }}
+		</div>
+	@endif
 	<h2>Cassie web client</h2>
 	<p class="lead">You need to login. <br>
 		For now you can only do it via you github account
