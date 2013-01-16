dd32e3684a84f722abe8c3c2ca573034a1647517
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-01-16 12:49:17 +0100

    base controller adds some assets

--- application/controllers/base.php
+++ application/controllers/base.php
@@ -2,6 +2,17 @@
 
 class Base_Controller extends Controller {
 
+
+	
+	public function __construct() {
+		Asset::add("bootstrap-css", "assets/bootstrap/css/bootstrap.min.css");
+		Asset::add("bootstrap-css-responsive", "assets/bootstrap/css/bootstrap-responsive.min.css");
+		Asset::add("style", "css/style.css");
+
+		parent::__construct();
+	}
+
+
 	/**
 	 * Catch-all method for requests that can't be matched.
 	 *
@@ -9,6 +20,7 @@ class Base_Controller extends Controller {
 	 * @param  array     $parameters
 	 * @return Response
 	 */
+	
 	public function __call($method, $parameters)
 	{
 		return Response::error('404');
